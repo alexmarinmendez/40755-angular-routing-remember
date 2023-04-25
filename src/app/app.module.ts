759541554e9634843897c899db10a2ed5a91f523
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-04-25 08:36:48 -0500

    feat: routing in app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './shared/components/login/login.component';
-import { AboutComponent } from './shared/components/about/about.component';
-import { MainRoutesModule } from './shared/modules/main-routes/main-routes.module';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, AboutComponent],
-  imports: [BrowserModule, MainRoutesModule],
+  declarations: [AppComponent, LoginComponent, AboutComponent, MainComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
